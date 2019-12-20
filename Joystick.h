0b16a17aedc9b693d9315c3efaab2b90a7c15b2f
Peter Gyongyosi <peter.gyongyosi@balabit.com>
2019-12-20 09:48:53 +0100

    improved logic for a 3-button controller

---
+++ Joystick.h
@@ -0,0 +1,91 @@
+#include <Arduino.h>
+
+#define isHigh(pin) (digitalRead(pin) == HIGH)
+
+/**
+ * Simple simulation of an 1-button analog joystick with buttons that tries to be
+ * more clever than just simply assigning the extremes when the buttons are
+ * pressed.
+ * 
+ * Initialize with the pin numbers, call readInputs() as frequently as possible
+ * and read the current status through the public attributes angle and buttonPressed.
+ * 
+ * It is absolutely not thread-safe.
+ * 
+ * FIXME: this is dummy at this point and just goes to the extremes
+ */
+class ThreeButtonJoystick
+{
+  public:
+    ThreeButtonJoystick(int _leftPin, int _middlePin, int _rightPin, int _acceleration = 90) {
+      leftPin = _leftPin;
+      middlePin = _middlePin;
+      rightPin = _rightPin;
+      acceleration = _acceleration;
+
+      pinMode(leftPin, INPUT);
+      pinMode(middlePin, INPUT);
+      pinMode(rightPin, INPUT);
+    }
+
+    void readInputs() {
+        // dummyInputLogic();
+        // return;
+
+        buttonPressed = isHigh(middlePin);
+
+        bool leftPressed = isHigh(leftPin);
+        bool rightPressed = isHigh(rightPin);
+
+        if ((!leftPressed && !rightPressed) || (leftPressed && rightPressed)) {
+          // if all buttons are pushed or released at the same time, stop immediately
+          preciseAngle = 0.0;
+          angle = 0;
+          return;
+        }
+
+        unsigned long int now = millis();
+
+        if (abs(preciseAngle) < 0.0001) {
+          directionStarted = now;
+          preciseAngle = (rightPressed ? 0.001 : -0.001);
+        } else {
+          // simple linear acceleration until +/- 90 degrees
+          preciseAngle = (rightPressed ? 1.0 : -1.0) * acceleration/1000.0 * (now - directionStarted);
+          preciseAngle = constrain(preciseAngle, -90, 90);
+
+          #ifdef DEBUG
+          Serial.print("angle: ");
+          Serial.print(angle);
+          Serial.print("floatangle: ");
+          Serial.print(preciseAngle);          
+          Serial.print("\t timeDiff: ");
+          Serial.println( (now-directionStarted) );
+          #endif
+
+          angle = (int) preciseAngle;
+        }
+
+    }
+
+    int angle;
+    bool buttonPressed;
+  
+  private:
+    int leftPin, middlePin, rightPin, acceleration;
+    unsigned long int directionStarted = 0;
+    float preciseAngle = 0.0;
+
+    void dummyInputLogic() {
+      // stupid dummy logic to test it: if both or none pressed, make it zero,
+      // otherwise just use the extremes -- remove when no longer needed
+      angle = 0;
+      if (isHigh(leftPin))
+        angle -= 90;
+      
+      if (isHigh(rightPin))
+        angle += 90;
+      
+      buttonPressed = isHigh(middlePin);
+    }
+};
--- TWANG.ino
+++ TWANG.ino
@@ -42,6 +42,7 @@
 #include "Lava.h"
 #include "Boss.h"
 #include "Conveyor.h"
+#include "Joystick.h"
 #include "settings.h"
 
 
@@ -1095,6 +1096,7 @@ void screenSaverTick(){
 // ---------------------------------
 // ----------- JOYSTICK ------------
 // ---------------------------------
+ThreeButtonJoystick myJoystick = ThreeButtonJoystick(2, 3, 4);
 void getInput(){
     // This is responsible for the player movement speed and attacking.
     // You can replace it with anything you want that passes a -90>+90 value to joystickTilt
@@ -1102,27 +1104,10 @@ void getInput(){
     // For example you could use 3 momentary buttons:
         // if(digitalRead(leftButtonPinNumber) == HIGH) joystickTilt = -90;
         // if(digitalRead(rightButtonPinNumber) == HIGH) joystickTilt = 90;
-        // if(digitalRead(attackButtonPinNumber) == HIGH) joystickWobble = ATTACK_THRESHOLD;
-    int16_t ax, ay, az;
-    int16_t gx, gy, gz;
-
-    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-    int a = (JOYSTICK_ORIENTATION == 0?ax:(JOYSTICK_ORIENTATION == 1?ay:az))/166;
-    int g = (JOYSTICK_ORIENTATION == 0?gx:(JOYSTICK_ORIENTATION == 1?gy:gz));
-
-    if(abs(a) < user_settings.joystick_deadzone) a = 0;
-    if(a > 0) a -= user_settings.joystick_deadzone;
-    if(a < 0) a += user_settings.joystick_deadzone;
-    MPUAngleSamples.add(a);
-    MPUWobbleSamples.add(g);
-
-    MPUAngleSamples.getMedian(a);
-    joystickTilt = (int8_t)a;
-    if(JOYSTICK_DIRECTION == 1) {
-        joystickTilt = 0-joystickTilt;
-    }
-    MPUWobbleSamples.getHighest(a);
-    joystickWobble = abs(a);
+        // if(digitalRead(attackButtonPinNumber) == HIGH) joystickWobble = ATTACK_THRESHOLD + 1;
+    myJoystick.readInputs();
+    joystickTilt = myJoystick.angle;
+    joystickWobble = myJoystick.buttonPressed ? ATTACK_THRESHOLD + 1 : 0;
 }
 
 // ---------------------------------
